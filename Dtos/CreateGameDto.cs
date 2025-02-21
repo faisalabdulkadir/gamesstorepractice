966efdd54e4dd31b37788dbd82bcd59638b3d4a7
faisalabdulkadir <wizardfaisal@gmail.com>
2025-02-21 19:48:37 +0100

    added validation to dto

--- Dtos/CreateGameDto.cs
+++ Dtos/CreateGameDto.cs
@@ -1,3 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameStore.Api.Dtos;
 
-public record class CreateGameDto(string Name, string Genre, DateOnly ReleaseDate, decimal Price);
+public record class CreateGameDto(
+    [Required][StringLength(50)] string Name,
+    [Required][StringLength(20)] string Genre,
+    DateOnly ReleaseDate,
+    [Range(1, 100)] decimal Price);
--- Dtos/UpdateGameDto.cs
+++ Dtos/UpdateGameDto.cs
@@ -1,3 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameStore.Api.Dtos;
 
-public record class UpdateGameDto(string Name, string Genre, DateOnly ReleaseDate, decimal Price);
+public record class UpdateGameDto([Required][StringLength(50)] string Name,
+    [Required][StringLength(20)] string Genre,
+    DateOnly ReleaseDate,
+    [Range(1, 100)] decimal Price);
